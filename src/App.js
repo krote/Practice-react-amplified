3cfe3ea77849c15188609ef8d955e46550dfe8f5
krote <krote0924@gmail.com>
2023-07-21 23:24:58 +0900

    add graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const initialState = { name: '', description: ''}
+
+const App = () => {
+  const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
+
+  useEffect( ()=> {
+    fetchTodos()
+  }, [])
+
+  function setInput(key, value){
+    setFormState({ ...formState, [key]: value })
+  }
+  
+}
+
+
 function App() {
   return (
     <div className="App">
